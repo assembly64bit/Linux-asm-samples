1103e7da52640d12315b1af172572859690e1802
Sample Linux Dev for train <nguyendinhtamlop9a4kysustt28@gmail.com>
2025-11-28 03:12:22 +0700

    Create connect.asm

---
+++ sample_network/connect.asm
@@ -0,0 +1,27 @@
+section .text
+        global _start
+_start:
+; SOCKET
+        mov rax, 41
+        mov rdi, 2
+        mov rsi, 1
+        xor rdx, rdx
+        syscall
+        xchg r12, rax
+
+        sub rsp, 16
+        mov word [rsp], 2
+        mov word [rsp+2], 0x5c11
+        mov dword [rsp+4], 0x1000007f
+        mov qword [rsp+8], 0
+; CONNECT
+        mov rax, 42
+        mov rdi, r12
+        lea rsi, [rsp]
+        mov rdx, 16
+        syscall
+
+; EXIT
+        mov rax, 60
+        xor rdi, rdi
+        syscall
